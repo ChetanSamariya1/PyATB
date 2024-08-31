7d0495468fb696878ebc84ed20da69dbc41acd50
Your Name <you@example.com>
2024-08-31 23:31:39 +0530

    Changes are done

--- src/ex_31082024/Lab109.py
+++ src/ex_31082024/Lab109.py
@@ -3,8 +3,9 @@
 # It will be automatically called when you create an Object
 
 class Dog():
-    name = None
+    name = None  # Instance Variable
     age = None
+  #  color = "Black" - This is hardcoded value - Not generic to all
 
     def __init__(self, name, age):
         # This is Constructor function. It does not return anything
@@ -19,6 +20,8 @@ class Dog():
 
 dog1 = Dog("Chow", 10)
 print(dog1.name)
+print(id(dog1))
 
 dog2 = Dog("Huskey", 8)
 print(dog2.name)
+print(id(dog2))
